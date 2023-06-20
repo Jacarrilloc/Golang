8cf65d58364a178b9a3d5aaf8404d5e8b6336cbe
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-20 12:16:46 -0500

    Uso de Modulos de Terceros de Github

---
+++ Modularización y Paquetes/Modulos Ajenos 5/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/donvito/hellomod"
+
+func main() {
+
+	hellomod.SayHello()
+
+	/*
+		cuadrado := figuras.Cuadrado{Lado: 8}
+		figuras.Medidas(&cuadrado)
+
+		circulo := figuras.Circulo{Radio: 15}
+		figuras.Medidas(&circulo)
+
+		p1 := models.Persona{}
+		p1.Constructor("Alex", 26)
+		fmt.Println(p1)
+		fmt.Println(p1.GetNombre())
+		p1.SetNombre("Julian")
+		fmt.Println(p1.GetNombre())
+		fmt.Println(p1)
+	*/
+}
