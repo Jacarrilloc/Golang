ffa2e46c203ea22e381dc72036c42ed604b9f9c7
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-20 15:21:59 -0500

    Testeo Profiling

--- 3 - Testing/4 - Profiling/TestUnitario/mate_test.go
+++ 3 - Testing/4 - Profiling/TestUnitario/mate_test.go
@@ -51,22 +51,3 @@ func TestGetMax(t *testing.T) {
 		}
 	}
 }
-
-func TestFibo(t *testing.T) {
-	tabla := []struct {
-		n int
-		r int
-	}{
-		{1, 1},
-		{8, 21},
-		{50, 12586269825},
-	}
-
-	for _, item := range tabla {
-		fibo := Fibonacci(item.n)
-
-		if fibo != item.r {
-			t.Errorf("GetMax incorrecta, tiene %d y se esperaba %d", fibo, item.r)
-		}
-	}
-}
