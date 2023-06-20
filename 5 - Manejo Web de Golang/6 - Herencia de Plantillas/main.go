de54e96e7357c87a6f3f00e4059a7731190566b1
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-20 18:14:57 -0500

    Herencia de Plantillas en Golang HTML

---
+++ 5 - Manejo Web de Golang/6 - Herencia de Plantillas/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Usuarios struct {
+	UserName string
+	Edad     int
+}
+
+func Saludar(nombre string) string {
+	return "Hola " + nombre + " desde una funcion"
+}
+
+func Index(rw http.ResponseWriter, r *http.Request) {
+
+	template, error := template.New("index.html").ParseFiles("index.html", "base.html")
+	Usuario := Usuarios{"Julian", 23}
+
+	if error != nil {
+		panic(error)
+	} else {
+		template.Execute(rw, Usuario)
+	}
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Index)
+
+	server := &http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
+
+	fmt.Println("Servidor corriendo en 3000")
+	fmt.Println("Run Server: http://localhost:3000/")
+	log.Fatal(server.ListenAndServe())
+}
