ae8ef7b19d1f2b15eb8ed5b53369e077ff101b2a
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-20 13:01:29 -0500

    Testeo Convencional en Golang

---
+++ 3 - Testing/2 - Test Convencional/TestUnitario/mate_test.go
@@ -0,0 +1,33 @@
+package testunitario
+
+import "testing"
+
+/*
+func TestSuma(t *testing.T) {
+	total := Suma(5, 5)
+
+	if total != 10 {
+		t.Errorf("Suma Incorrecta, tiene %d y se eséraba %d", total, 10)
+	}
+}
+*/
+
+func TestSuma(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}{
+		{1, 2, 3},
+		{2, 2, 4},
+		{25, 25, 50},
+	}
+
+	for _, item := range tabla {
+		total := Suma(item.a, item.b)
+
+		if total != item.c {
+			t.Errorf("Suma incorrecta, tiene %d y se esperaba %d", total, item.c)
+		}
+	}
+}
