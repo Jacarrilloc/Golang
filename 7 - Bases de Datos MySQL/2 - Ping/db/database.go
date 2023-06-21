18250580232eeb729f3dc2f74ccbab6e1de7157b
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-21 14:00:28 -0500

    Implementacion de Funcion que Verifica la coneccion con la base de datos

---
+++ 7 - Bases de Datos MySQL/2 - Ping/db/database.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//username:password@tcp(localhost:3308)/database
+
+const url = "root:admin@tcp(localhost:3308)/goweb_db"
+
+// Guarda la Conexion
+var db *sql.DB
+
+// Funcion que abre la conexion con la base de datos
+func Connect() {
+	conection, error := sql.Open("mysql", url)
+	if error != nil {
+		panic(error)
+	}
+
+	fmt.Println("Conexion Exitosa ")
+	db = conection
+}
+
+// Funcion que cierra la conexion
+func Close() {
+	db.Close()
+}
+
+// Funcion que verifica que este abierta la conexion
+func Ping() {
+	if error := db.Ping(); error != nil {
+		panic(error)
+	}
+}
