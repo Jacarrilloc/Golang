21501fa9b2e9f4afcbcbf29338afd04a05a26af5
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-22 16:25:45 -0500

    Delete un Registro

---
+++ 8 - API REST/10 - Eliminar un Registro/handler/handler.go
@@ -0,0 +1,93 @@
+package handler
+
+import (
+	"apirest/db"
+	"apirest/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func GetUsers(rw http.ResponseWriter, r *http.Request) {
+
+	rw.Header().Set("Content-Type", "application/json")
+
+	db.Connect()
+	users := models.ListUsers()
+	db.Close()
+	output, _ := json.Marshal(users)
+	fmt.Fprintln(rw, string(output))
+}
+
+func GetUser(rw http.ResponseWriter, r *http.Request) {
+
+	rw.Header().Set("Content-Type", "application/json")
+
+	//Obtener ID
+	vars := mux.Vars(r)
+	userId, _ := strconv.Atoi(vars["id"])
+
+	db.Connect()
+	user := models.GetUser(userId)
+	db.Close()
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
+}
+
+func CreateUser(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+
+	//Obtener Registro
+	user := models.User{}
+	decoder := json.NewDecoder(r.Body)
+
+	if error := decoder.Decode(&user); error != nil {
+		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Connect()
+		user.Save()
+		db.Close()
+	}
+
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
+}
+
+func UpdateUser(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+
+	//Obtener Registro
+	user := models.User{}
+	decoder := json.NewDecoder(r.Body)
+
+	if error := decoder.Decode(&user); error != nil {
+		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Connect()
+		//Como el metodo user.Save funciona para crear o actualizar la información, es posible usar el mismo codigo de CreateUser
+		user.Save()
+		db.Close()
+	}
+
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
+}
+
+func DeleteUser(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+
+	//Obtener ID
+	vars := mux.Vars(r)
+	userId, _ := strconv.Atoi(vars["id"])
+
+	db.Connect()
+	user := models.GetUser(userId)
+	user.Delete()
+	db.Close()
+
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
+}
