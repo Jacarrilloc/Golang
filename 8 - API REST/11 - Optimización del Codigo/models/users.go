72b9d935ce55d8a36b19979dca1533df226e54a0
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-22 16:36:07 -0500

    Refactorizacion del código para la optimización

---
+++ 8 - API REST/11 - Optimización del Codigo/models/users.go
@@ -0,0 +1,90 @@
+package models
+
+import "apirest/db"
+
+type User struct {
+	Id       int64  `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type Users []User
+
+const UserSchema string = `CREATE TABLE users (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(30) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(50),
+	create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`
+
+// Constructor de User
+func NewUser(username, password, email string) *User {
+	user := &User{Username: username, Password: password, Email: email}
+	return user
+}
+
+// Crear Usuario e instertar a la base de Datos
+func CreateUser(username, password, email string) *User {
+	user := NewUser(username, password, email)
+	user.Save()
+	return user
+}
+
+// Insertar Un registro a la Base de Datos
+func (user *User) insert() {
+	sql := "INSERT users SET username=?, password=?, email=?"
+	result, _ := db.Exec(sql, user.Username, user.Password, user.Email)
+	user.Id, _ = result.LastInsertId()
+}
+
+// Listar Todos los Registros
+func ListUsers() (Users, error) {
+	sql := "SELECT id, username, password, email FROM users"
+	users := Users{}
+	rows, error := db.Query(sql)
+
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+		users = append(users, user)
+	}
+	return users, error
+}
+
+// Obtener un Registro
+func GetUser(id int) (*User, error) {
+	user := NewUser("", "", "")
+	sql := "SELECT id, username, password, email FROM users WHERE id=?"
+	if rows, error := db.Query(sql, id); error != nil {
+		return nil, error
+	} else {
+		for rows.Next() {
+			rows.Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+		}
+
+		return user, nil
+	}
+}
+
+// Actualiar un registro
+func (user *User) update() {
+	sql := "UPDATE users SET username=?, password=?, email=? WHERE id=?"
+	db.Exec(sql, user.Username, user.Password, user.Email, user.Id)
+}
+
+// Guardar o Editar Registro
+func (user *User) Save() {
+	if user.Id == 0 {
+		user.insert()
+	} else {
+		user.update()
+	}
+}
+
+// Eliminar un Registro
+func (User *User) Delete() {
+	sql := "DELETE FROM users WHERE id=?"
+	db.Exec(sql, User.Id)
+}
