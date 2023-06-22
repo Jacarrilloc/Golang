64571ebc0b1f9756f5b9d1756d36d2ca5cd5c8aa
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-22 15:32:36 -0500

    Listar los Registros con API REST

---
+++ 8 - API REST/3 - Listar Registros/handler/handler.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"apirest/db"
+	"apirest/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func GetUsers(rw http.ResponseWriter, r *http.Request) {
+	db.Connect()
+	users := models.ListUsers()
+	db.Close()
+	output, _ := json.Marshal(users)
+	fmt.Fprintln(rw, string(output))
+}
+
+func GetUser(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Obtener un Usuario")
+}
+
+func CreateUser(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Crear un Usuario")
+}
+
+func UpdateUser(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Editar un Usuario")
+}
+
+func DeleteUser(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Eliminar un Usuario")
+}
